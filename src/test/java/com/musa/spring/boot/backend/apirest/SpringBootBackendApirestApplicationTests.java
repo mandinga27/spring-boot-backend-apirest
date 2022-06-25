c27b4640fcf320559165b5f6ca2a8c743143d370
Abraham Musa <amusa26@gmail.com>
2022-06-25 17:37:10 -0400

    first commit

---
+++ src/test/java/com/musa/spring/boot/backend/apirest/SpringBootBackendApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musa.spring.boot.backend.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBackendApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
