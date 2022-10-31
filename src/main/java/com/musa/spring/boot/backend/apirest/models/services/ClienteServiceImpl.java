40a0f902d72aa472f5b93cbaa061a1d2e9449aa7
Abraham Musa <amusa26@gmail.com>
2022-10-31 11:42:23 -0300

    se agrega service decorador y transactional read only

--- src/main/java/com/musa/spring/boot/backend/apirest/models/services/ClienteServiceImpl.java
+++ src/main/java/com/musa/spring/boot/backend/apirest/models/services/ClienteServiceImpl.java
@@ -3,16 +3,20 @@ package com.musa.spring.boot.backend.apirest.models.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.musa.spring.boot.backend.apirest.models.dao.IClienteDao;
 import com.musa.spring.boot.backend.apirest.models.entity.Cliente;
 
+@Service //tipo de estereotipo de component, la decora, es un beans
 public class ClienteServiceImpl implements IClienteService{
 
-	@Autowired //autowired inyeccion de dependencias
+	@Autowired //autowired inyeccion de dependencias, cliente dao
 	private IClienteDao clienteDao;
 	
 	@Override 
+	@Transactional(readOnly = true)
 	public List<Cliente> findAllList() {
 		// TODO Auto-generated method stub
 		return (List<Cliente>) clienteDao.findAll();
