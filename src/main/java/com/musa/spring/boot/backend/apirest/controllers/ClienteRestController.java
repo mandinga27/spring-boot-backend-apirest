61ba4346e5bc0bd92f343a144ee3049fa3dc9cf1
Abraham Musa <amusa26@gmail.com>
2022-11-01 21:18:40 -0300

    se agrega cors

--- src/main/java/com/musa/spring/boot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/musa/spring/boot/backend/apirest/controllers/ClienteRestController.java
@@ -5,11 +5,12 @@ import java.util.List;
 import com.musa.spring.boot.backend.apirest.models.entity.Cliente;
 import com.musa.spring.boot.backend.apirest.models.services.IclienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api") //url de primer nivel
 public class ClienteRestController {
